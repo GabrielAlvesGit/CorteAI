92f6355edcb8cdd0653d9d91ee711321590533b4
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-07-24 14:21:49 -0300

    End testtimonials + ajuste em about + create initial scheduling

--- partials/Home/testimonials.php
+++ partials/Home/testimonials.php
@@ -1,25 +1,58 @@
 <section class="pgHome__testimonials">
-        <div class="container">
-            <h3 class="pgHome__testimonials__title">O que dizem nossos clientes</h3>
+    <div class="container">
+        <h3 class="pgHome__testimonials__title">O que dizem nossos clientes</h3>
 
-            <div class="pgHome__testimonials__cads">
-                <div class="pgHome__testimonials__cards__card">
-                    <div class="pgHome__testimonials__cards__card__user">
-                        <div class="pgHome__testimonials__cards__card__user__img">
-                            <img class="pgHome__about__content__left__img" src="<?php echo get_theme_file_uri('/img/user/user-barbe.jpg'); ?>" alt="about">
-                        </div>
-
-                        <div class="pgHome__testimonials__cards__card__user__info">
-                            <span class="pgHome__testimonials__cards__card__user__info__name">Ricardo Oliveira</span>
-                            <p class="pgHome__testimonials__cards__card__user__info__date">Cliente há 3 anos</p>
-                        </div>
+        <div class="pgHome__testimonials__cards">
+            <div class="pgHome__testimonials__cards__card">
+                <div class="pgHome__testimonials__cards__card__user">
+                    <div class="pgHome__testimonials__cards__card__user__img">
+                        <img class="pgHome__about__content__left__img" src="http://localhost:10022/wp-content/themes/CorteA%C3%AD/img/user/user-barbe.jpg" alt="about">
                     </div>
 
-                    <div class="pgHome__testimonials__cards__card__description">
-                        <i class="ri-double-quotes-l"></i>
-                        Frequento a Barbearia Moderna desde a inauguração e nunca me decepcionei. O atendimento é impecável e os cortes sempre ficam perfeitos. Recomendo a todos!
+                    <div class="pgHome__testimonials__cards__card__user__info">
+                        <span class="pgHome__testimonials__cards__card__user__info__name">Ricardo Oliveira</span>
+                        <p class="pgHome__testimonials__cards__card__user__info__date">Cliente há 3 anos</p>
                     </div>
+                </div>
+
+                <div class="pgHome__testimonials__cards__card__description">
+                    <p class="pgHome__testimonials__cards__card__description__text">Frequento a Barbearia Moderna desde a inauguração e nunca me decepcionei. O atendimento é impecável e os cortes sempre ficam perfeitos. Recomendo a todos!</p> 
+                </div>
+            </div>
+
+            <div class="pgHome__testimonials__cards__card">
+                <div class="pgHome__testimonials__cards__card__user">
+                    <div class="pgHome__testimonials__cards__card__user__img">
+                        <img class="pgHome__about__content__left__img" src="http://localhost:10022/wp-content/themes/CorteA%C3%AD/img/user/user-barbe.jpg" alt="about">
+                    </div>
+
+                    <div class="pgHome__testimonials__cards__card__user__info">
+                        <span class="pgHome__testimonials__cards__card__user__info__name">Ricardo Oliveira</span>
+                        <p class="pgHome__testimonials__cards__card__user__info__date">Cliente há 3 anos</p>
+                    </div>
+                </div>
+
+                <div class="pgHome__testimonials__cards__card__description">
+                    <p class="pgHome__testimonials__cards__card__description__text">Frequento a Barbearia Moderna desde a inauguração e nunca me decepcionei. O atendimento é impecável e os cortes sempre ficam perfeitos. Recomendo a todos!</p> 
+                </div>
+            </div>
+
+            <div class="pgHome__testimonials__cards__card">
+                <div class="pgHome__testimonials__cards__card__user">
+                    <div class="pgHome__testimonials__cards__card__user__img">
+                        <img class="pgHome__about__content__left__img" src="http://localhost:10022/wp-content/themes/CorteA%C3%AD/img/user/user-barbe.jpg" alt="about">
+                    </div>
+
+                    <div class="pgHome__testimonials__cards__card__user__info">
+                        <span class="pgHome__testimonials__cards__card__user__info__name">Ricardo Oliveira</span>
+                        <p class="pgHome__testimonials__cards__card__user__info__date">Cliente há 3 anos</p>
+                    </div>
+                </div>
+
+                <div class="pgHome__testimonials__cards__card__description">
+                    <p class="pgHome__testimonials__cards__card__description__text">Frequento a Barbearia Moderna desde a inauguração e nunca me decepcionei. O atendimento é impecável e os cortes sempre ficam perfeitos. Recomendo a todos!</p> 
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
