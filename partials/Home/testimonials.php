2880422d04b7f2a85bf7db95f2f1fea05c0e3b70
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-07-24 13:48:45 -0300

    Renomeia variáveis globais de fonte para seguir padrão de nomenclatura consistente

--- partials/Home/testimonials.php
+++ partials/Home/testimonials.php
@@ -1,19 +1,21 @@
 <section class="pgHome__testimonials">
         <div class="container">
-            <h2 class="pgHome__testimonials__title">O que dizem nossos clientes</h2>
+            <h3 class="pgHome__testimonials__title">O que dizem nossos clientes</h3>
 
             <div class="pgHome__testimonials__cads">
                 <div class="pgHome__testimonials__cards__card">
-                    <div class="testimonial-header">
-                        <div class="client-img">
-                            <i class="ri-user-line"></i>
+                    <div class="pgHome__testimonials__cards__card__user">
+                        <div class="pgHome__testimonials__cards__card__user__img">
+                            <img class="pgHome__about__content__left__img" src="<?php echo get_theme_file_uri('/img/user/user-barbe.jpg'); ?>" alt="about">
                         </div>
-                        <div class="client-info">
-                            <h4>Ricardo Oliveira</h4>
-                            <p>Cliente há 3 anos</p>
+
+                        <div class="pgHome__testimonials__cards__card__user__info">
+                            <span class="pgHome__testimonials__cards__card__user__info__name">Ricardo Oliveira</span>
+                            <p class="pgHome__testimonials__cards__card__user__info__date">Cliente há 3 anos</p>
                         </div>
                     </div>
-                    <div class="testimonial-text">
+
+                    <div class="pgHome__testimonials__cards__card__description">
                         <i class="ri-double-quotes-l"></i>
                         Frequento a Barbearia Moderna desde a inauguração e nunca me decepcionei. O atendimento é impecável e os cortes sempre ficam perfeitos. Recomendo a todos!
                     </div>
