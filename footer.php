3fe243ed6f6694cbe06948fd5ce5a72ae3be3992
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2025-07-24 16:12:13 -0300

    create init footer

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 </main>
 
 <!-- Inicio Wordpress Footer -->
-<?php wp_footer(); ?>
+<?php 
+ wp_footer(); 
+  get_template_part('partials/Global/footer');
+?>
 <!-- Final Wordpress Footer -->
 </body>
 
---
+++ partials/Global/footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer__top">
+            <address class="footer__top__address">
+                You can contact author at
+                <a href="http://www.example.com/contact">www.example.com</a>.<br />
+                If you see any bugs, please
+                <a href="mailto:webmaster@example.com">contact webmaster</a>.<br />
+                You may also want to visit us:<br />
+                Mozilla Foundation<br />
+                331 E Evelyn Ave<br />
+                Mountain View, CA 94041<br />
+            </address>
+
+            <div class="footer__top__navigation">
+                <ul class="footer__top__navigation__list">
+                    <li class="footer__top__navigation__list__item">
+                        <a href="#" class="footer__top__navigation__list__item__link">Lorem</a>
+
+                        <ul class="subMenu">
+                            <li class="subMenu__item">
+                                <a href="#" class="subMenu__item__link">Lorem Filho</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer__bottom">
+            <p class="footer__bottom__copyright">© 2025 Copyright.<br>Todos os direitos reservados</p>
+
+            <a href="#" target="_blank" class="footer__bottom__poweredBy">Powered by <b>LOREM</b></a>
+
+
+            <ul class="footer__bottom__policyPrivacy">
+                <li>
+                    <a href="" target="_blank">Política de Privacidade</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">Termos e Condições de Uso</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
